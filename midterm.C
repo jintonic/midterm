5833a861f109e980e84ee1fe146135d8d68f7f2b
Jing Liu <jintonic@physino.xyz>
2023-03-22 13:16:34 -0500

    added

---
+++ midterm.C
@@ -0,0 +1,10 @@
+{
+	const int n = 13;
+	int scores[n] = {-39, -97, -111, -91, -58, -120, -96, -86, -74, -79, -68, -66, -90};
+
+	TH1I h1("h1","midterm score distribution;score;entries",5,-150,0);
+	for (int i=0; i<n; i++) {
+		h1.Fill(scores[i]);
+	}
+	h1.Draw();
+}
